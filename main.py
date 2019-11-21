0e147607fd12939e51f9564f38ae739646e09861
Fabi138 <fabian.burgener95@gmail.com>
2019-11-21 15:52:52 +0100

    updated main.py for new html

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ from flask import request
 from libs import journey_log
 from datetime import datetime
 
+
 app = Flask("Track your day")
 
 @app.route("/")
 @app.route("/index")
 def index():
+    return render_template("index.html")
+
+@app.route("/list/journey")
+def list_journey():
     trips = journey_log.read_trips()
-    return render_template("index.html", reisen = trips)
+    chart = journey_log.pie_chart()
+    return render_template("list_journey.html", reisen = trips, viz_div=chart)
+
 
 @app.route("/add/journey", methods=['GET', 'POST'])
 def add_journey():
     if request.method == 'POST':
         journey_log.save_new_trip(request.form)
-        return redirect("/index")
+        return redirect("/list/journey")
 
     return render_template("add_journey.html")
 
