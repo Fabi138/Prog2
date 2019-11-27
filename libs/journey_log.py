b2fac15287da2e5c54c5437e8aa2219c4f8e80d4
Fabi138 <fabian.burgener95@gmail.com>
2019-11-27 09:44:17 +0100

    logbuch backend reise

--- libs/journey_log.py
+++ libs/journey_log.py
@@ -1,28 +1,53 @@
 from datetime import date
+from datetime import datetime
 import json
+from plotly.offline import plot
+import plotly.graph_objects as go
 
 def save_new_trip(form_request):
-    print(form_request)
+    """Summary
+    Add a new trip to the log
+    Args:
+        form_request (request.form): form of add_journey.html
+    """
+    #print(form_request)
     today = date.today()
-    current_date = today.strftime("%d%m%Y")
+    current_date = today.strftime("%d%m%Y") #need current date for key in dict
     
+    start_zeit = form_request.get('start_zeit')
+    ziel_zeit = form_request.get('ziel_zeit')
+
+    start = datetime.strptime(start_zeit, '%H:%M') #string to date for calculation
+    ziel = datetime.strptime(ziel_zeit, '%H:%M') #string to date for calculation
+    
+    travel_time = ziel - start  
+    travel_time_minutes = int(travel_time.total_seconds()/60) #convert from hh:mm to minutes
+
     reise = read_trips()
-    reise[current_date]  = []
-    reise[current_date].append(
+    current_date_data = reise.get(current_date, []) #get the current date entry
+    current_date_data.append(
         {
             "reisemittel": form_request.get('reise_art'),
             "von": form_request.get('von'),
             'bis': form_request.get('bis'),
-            'start_zeit': form_request.get('start_zeit'),
-            'ziel_zeit': form_request.get('ziel_zeit'),
-            'verspaetung': form_request.get('verspaetung', 0)
+            'start_zeit': start_zeit,
+            'ziel_zeit': ziel_zeit,
+            'verspaetung': form_request.get('verspaetung', 0),
+            'travel_time': travel_time_minutes
         }
     )
 
+    reise[current_date] = current_date_data
+    
     with open('reise_log_buch.txt', "w", encoding="utf-8") as open_file:
         json.dump(reise, open_file)
 
 def read_trips():
+    """Summary
+    read all the data from the logs
+    Returns:
+        dict: all journeys 
+    """
     reise = {}
     try:
         with open('reise_log_buch.txt', "r") as open_file:
@@ -31,3 +56,25 @@ def read_trips():
         print("Error with file at reise_log_buch.txt!")
     finally:
         return reise
+
+def pie_chart():
+    """Summary
+    generate pie chart for time per vehical
+    Returns:
+        String: html of chart
+    """
+    labels = ['Flugzeug','Auto','Zug']
+    time = {'flugzeug': 0, 'auto': 0, 'zug': 0}
+
+    data = read_trips()
+
+    for key, reise in data.items(): #for everyday 
+        for trip in reise: #for every trip in day
+            time[trip['reisemittel']] = time[trip['reisemittel']] + trip['travel_time']
+
+    #print(time)
+    values = [time['flugzeug'], time['auto'], time['zug']] #create valuelist for chart
+
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    div = plot(fig, output_type="div")
+    return div
