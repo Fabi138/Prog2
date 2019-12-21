bce8c5b37ea79d9f7d0264aff0d212093c8e7739
Fabi138 <fabian.burgener95@gmail.com>
2019-12-21 13:18:49 +0100

    new functions in main

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from flask import redirect
 from flask import request
 
 from libs import journey_log
-from datetime import datetime
+from libs import game_log
+from libs import file_helper
 
+from datetime import datetime
 
 app = Flask("Track your day")
 
@@ -31,12 +33,25 @@ def add_journey():
 
 @app.route("/list/game")
 def list_game():
-    return render_template("add_game.html")
-
+    games_data = game_log.read_games()
+    chart = game_log.pie_chart()
+    return render_template("list_game.html", games = games_data, viz_div=chart)
 
 @app.route("/add/game", methods=['GET', 'POST'])
 def add_game():
-    return render_template("add_game.html")
+    if request.method == 'POST':
+        game_log.save_new_game(request.form)
+        return redirect("/list/game")
+    my_games = game_log.read_my_games()
+    return render_template("add_game.html", games=my_games)
+
+@app.route("/manage/game", methods=['GET', 'POST'])
+def manage_games():
+    if request.method == 'POST':
+        game_log.add_game(request.form)
+
+    my_games = game_log.read_my_games()
+    return render_template("manage_game.html", games=my_games)
 
 @app.template_filter('datetime')
 def format_date_for_index(value):
